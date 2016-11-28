9f111e673303764a06e6d08818e80ec9116642c0
t103360452 <t103360452@ntut.org.tw>
2016-11-28 21:33:55 +0800

    模組化，解析src部分

--- 解析網址/parsing_url.c
+++ 解析網址/parsing_url.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h> 
+int get_num_url(char *total_url);
+	/*用途:計算已解析出來網址的數量
+	*total_url:已解析出來的陣列 */
+
+void parsing_request_all_URL(char *server_respone,char **result_parsing_url);
+	/* 用途： 解析網頁裡頭所有相對路徑檔案網址 
+		server_respone	: 要被解析出網址的陣列 ,資料型態:一維陣列 
+		result_parsing_url:   把解析出來的網址存放在該位址 ,資料型態:二維陣列  且宣告格式: char 陣列名稱[i][200]; i為任意整數，視解析出來的網址大概數量做決定 
+	*/
+void parsing_request_one_URL(char *k,char **one_url);
+	/*用途: 給 parsing_request_all_URL(char *server_respone,char *total_url)用，只解析1組網址
+		 char *k  :要被解析出網址的陣列 ,資料型態:一維陣列
+		 one_url  :解析出來的網址存放在該位址 ,資料型態:一維陣列 
+	 */
+
+
+int main()
+{
+    char hello[] = "src=\"/V7/js/jquery-ui-1.8.20.custom.min.js\"></script><script type=\"text/javascript\" src=\"/V7/js/funcTable.js\"></script><script type=\"text/javascript\" src=\"/V7/js/stone.js\"></script><script type=\"text/javascript\" src=\"/V7/js/cwb.js\"></script><script type=\"text/javascript\" src=\"/V7/js/warning_info.js\"></script><script type=\"text/javascript\" src=\"/V7/js/warning.js\"></script><script type=\"text/javascript\" src=\"/V7/js/scrolltopcontrol.js\"></script><script language=\"JavaScript\" src=\"/V7/js/HotSearch.js\"></script><script language=\"JavaScript\" src=\"/V7/js/jquery.jsonSuggest.js\"></script>";
+ 	char k[100][200];
+ 	int i,url_num;
+	char *hello_test="HTTP/1.1\r\nHost:";
+	char *request = (char *)malloc(sizeof(char) * (11));
+	parsing_request_all_URL(hello,k); //把hello內容解析出網址給k 
+
+	url_num=get_num_url(k);    //取得被解析出來的url數量 
+	
+	printf("sum=%d\n",url_num);
+	
+	for(i=0;i<url_num;i++)
+		printf("%s\n",k[i]);
+	printf("\n\n\n%s",hello_test);
+    return 0;
+}
+
+
+
+
+
+
+
+
 int get_num_url(char *total_url)
 	{
 		int num=0, sum=0;
@@ -12,93 +54,53 @@ int get_num_url(char *total_url)
 	return num;
 	}
 	
-void parsing_request_one_URL(char *k,char *one_url)
+void parsing_request_one_URL(char *k,char **one_url)
 {
-	char *front_url,*end_url;
-	char *a="href=\"/";
-	char *b="href='/";
-	char *c="src=\"/";
-	char *d="src='/";
+	char *front_ptr_url,*end_ptr_url;
+	char *src_double_quotes="src=\"/";
+	char *src_apostrophe="src='/";
 		
-	if((strncmp(k,a,7)==0)||(strncmp(k,c,6)==0))
+	if(strncmp(k,src_double_quotes,6)==0)
 		{
-			front_url=strstr(k,"=\"");
-			front_url+=2;
-			end_url=strchr(front_url,'\"');
-			memcpy(one_url,front_url,end_url-front_url);
+			front_ptr_url=strstr(k,"=\"");
+			front_ptr_url+=2;
+			end_ptr_url=strchr(front_ptr_url,'\"');
+			memcpy(one_url,front_ptr_url,end_ptr_url-front_ptr_url);
 		}
 		
-	else if((strncmp(k,b,7)==0)||(strncmp(k,d,6)==0))
+	else if(strncmp(k,src_apostrophe,6)==0)
 		{
-			front_url=strstr(k,"='");
-			front_url+=2;
-			end_url=strchr(front_url,'\'');
-			memcpy(one_url,front_url,end_url-front_url);
+			front_ptr_url=strstr(k,"='");
+			front_ptr_url+=2;
+			end_ptr_url=strchr(front_ptr_url,'\'');
+			memcpy(one_url,front_ptr_url,end_ptr_url-front_ptr_url);
 		}	
 }
-void parsing_request_all_URL(char *server_respone,char *total_url)
+void parsing_request_all_URL(char *server_respone,char **result_parsing_url)
 {
 	int count=0;
-	char *front_url;
+	char *front_ptr_url;
 	   
-	char *a="href=\"/";
-	char *b="href='/";
-	char *c="src=\"/";
-	char *d="src='/";
+	char *src_double_quotes="src=\"/";
+	char *src_apostrophe="src='/";
 	
-	front_url=strstr(server_respone,b);			
-	while(front_url!=NULL){
-		parsing_request_one_URL(front_url,&total_url[count]);
-		count+=200; 
-		front_url+=1;
-		front_url=strstr(front_url,b);
-	}
 	
-	front_url=strstr(server_respone,c);			
-	while(front_url!=NULL){  //main
+	front_ptr_url=strstr(server_respone,src_double_quotes);			
+	while(front_ptr_url!=NULL){  
 	
-		parsing_request_one_URL(front_url,&total_url[count]);
+		parsing_request_one_URL(front_ptr_url,&result_parsing_url[count]);
 		count+=200;                    //二維陣列,前往下一個二微陣列 
-		front_url+=1;
-		front_url=strstr(front_url,c);
+		front_ptr_url+=1;
+		front_ptr_url=strstr(front_ptr_url,src_double_quotes);
 	}
 	
-	front_url=strstr(server_respone,d);			
-	while(front_url!=NULL){
-		parsing_request_one_URL(front_url,&total_url[count]);
+	front_ptr_url=strstr(server_respone,src_apostrophe);			
+	while(front_ptr_url!=NULL){
+		parsing_request_one_URL(front_ptr_url,&result_parsing_url[count]);
 		count+=200; 
-		front_url+=1;
-		front_url=strstr(front_url,d);
+		front_ptr_url+=1;
+		front_ptr_url=strstr(front_ptr_url,src_apostrophe);
 	}
 	
-	front_url=strstr(server_respone,a);			
-	while(front_url!=NULL){
-		parsing_request_one_URL(front_url,&total_url[count]);
-		count+=200; 
-		front_url+=1;
-		front_url=strstr(front_url,a);
-	}
-}
 
-int main(int argc, char **argv)
-{
-     char hello[] = "src=\"/V7/js/jquery-ui-1.8.20.custom.min.js\"></script><script type=\"text/javascript\" src=\"/V7/js/funcTable.js\"></script><script type=\"text/javascript\" src=\"/V7/js/stone.js\"></script><script type=\"text/javascript\" src=\"/V7/js/cwb.js\"></script><script type=\"text/javascript\" src=\"/V7/js/warning_info.js\"></script><script type=\"text/javascript\" src=\"/V7/js/warning.js\"></script><script type=\"text/javascript\" src=\"/V7/js/scrolltopcontrol.js\"></script><script language=\"JavaScript\" src=\"/V7/js/HotSearch.js\"></script><script language=\"JavaScript\" src=\"/V7/js/jquery.jsonSuggest.js\"></script>";
-     char *ptr,*eptr; 
-     int i=0,y;
-	 
-    char k[500][200];
-    
-
-	parsing_request_all_URL(hello,&k);	
-	y=get_num_url(k);
-	printf("%d\n",y);
-	for(i=0;i<y;i++)
-	{
-			
-			if(k[i]== '\0')
-			 printf("%d is empty \n",i);
-			else
-			 printf("i=%d %s\n",i,k[i]);
-	}	
-    return 0;
 }
